7b802af99374bb30d4ed336c3a1142522433c9b2
milad karimi <izi.bizines@gmail.com>
2023-01-24 10:44:47 +0330

    Task Model created

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,11 +4,26 @@ mongoose.set('strictQuery', false);
 
 const db = mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
 
-const User = mongoose.model('User', { name: String, age: Number });
+// const User = mongoose.model('User', { name: String, age: Number });
 
-const me = new User({ name: 'esi', age: '37' });
+// const me = new User({ name: 'esi', age: '37' });
 
-me.save()
-    .then((me) => console.log('success', me))
+// me.save()
+//     .then((me) => console.log('success', me))
+//     .catch(error => console.log('error', error));
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+});
+
+const task = new Task({ description: 'Learn the Mongoose library', completed: false });
+
+task.save()
+    .then(() => console.log('success', task))
     .catch(error => console.log('error', error));
 
