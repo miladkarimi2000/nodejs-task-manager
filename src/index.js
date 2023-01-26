1d613613e29ade474bc0a540387d1ee003fcb277
milad karimi <izi.bizines@gmail.com>
2023-01-26 09:49:43 +0330

    11. Resource Reading Endpoints Part II

--- src/index.js
+++ src/index.js
@@ -56,6 +56,30 @@ app.post('/tasks', (req, res) => {
         });
 });
 
+app.get('/tasks', (req, res) => {
+    Task.find()
+        .then(tasks => {
+            res.send(tasks);
+        })
+        .catch(err => {
+            res.status(500).send(err);
+        });
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+    Task.findById(_id)
+        .then(task => {
+            if (!task) {
+                return res.status(404).send('task not found');
+            }
+            res.send(task)
+        })
+        .catch(err => {
+            res.status(500).send(err);
+        });
+});
+
 app.listen(port, () => {
     console.log('Server in running on port ', port)
 });
