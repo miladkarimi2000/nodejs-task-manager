a1183e426fe30cc8a3eee08a17c9b62a7d9fbcc5
milad karimi <izi.bizines@gmail.com>
2023-02-16 10:25:44 +0330

    4. Logging in Users

--- src/models/user.js
+++ src/models/user.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
-
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -11,6 +10,7 @@ const userSchema = mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -43,6 +43,19 @@ const userSchema = mongoose.Schema({
     }
 });
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new Error('Unable to login');
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        throw new Error('Unable to login');
+    }
+    return user;
+};
+
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     const user = this;
     if (user.isModified('password')) {
