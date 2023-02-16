a2e0728d84a8c026c8177e0243bada347b5a5537
milad karimi <izi.bizines@gmail.com>
2023-02-16 20:02:07 +0330

    8. Accepting Authentication Tokens

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -23,15 +24,10 @@ router.post('/users/login', async (req, res) => {
     } catch (e) {
         res.status(400).send(e);
     }
-})
+});
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (e) {
-        res.status(500).send(e);
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user);
 });
 
 router.get('/users/:id', async (req, res) => {
