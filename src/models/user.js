1fb25e51bb13653df043e3bd32c3edf0f3c821ca
milad karimi <izi.bizines@gmail.com>
2023-02-16 15:14:53 +0330

    6. Generating Authentication Tokens

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,7 +31,6 @@ const userSchema = mongoose.Schema({
                 throw new Error('Password cannot contain "password"');
             }
         }
-
     },
     age: {
         type: Number,
@@ -40,8 +40,22 @@ const userSchema = mongoose.Schema({
                 throw new Error('Age must be a positive number');
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewtoken');
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+    return token;
+};
 
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email });
--- src/routers/user.js
+++ src/routers/user.js
@@ -6,8 +6,9 @@ const router = new express.Router();
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
     try {
-        const result = await user.save();
-        res.status(201).send(result);
+        await user.save();
+        const token = await user.generateAuthToken();
+        res.status(201).send({ user, token });
     } catch (e) {
         res.status(400).send(e);
     }
@@ -17,7 +18,8 @@ router.post('/users/login', async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await User.findByCredentials(email, password);
-        res.send(user);
+        const token = await user.generateAuthToken();
+        res.send({ user, token });
     } catch (e) {
         res.status(400).send(e);
     }
