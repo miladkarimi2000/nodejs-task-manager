c0aa14b3f5ef4d95c4e5f7cc82b5d39a3c5c45bd
milad karimi <izi.bizines@gmail.com>
2023-02-18 19:17:45 +0330

    14. Authenticating Task Endpoints

--- src/index.js
+++ src/index.js
@@ -8,26 +8,9 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-// app.use(express.json());
-// app.use(userRouter);
-// app.use(taskRouter);
-
-
-
-const User = require('./models/user');
-const Task = require('./models/task');
-
-app.get('/test', async (req, res) => {
-
-    // const task = await Task.findById('63f08bd4c636ff6d943d961c');
-    // await task.populate('owner');
-    // res.send(task);
-
-    const user = await User.findById('63f0815327cf25c1823999d2');
-    await user.populate('tasks');
-    console.log(user);
-    res.send(user.tasks);
-});
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(port, () => {
     console.log('Server in running on port ', port)
