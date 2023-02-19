0893a5e24e8a2ab06f84865fc623b1207638789b
milad karimi <izi.bizines@gmail.com>
2023-02-19 13:51:26 +0330

    3. Filtering Data

--- src/models/task.js
+++ src/models/task.js
@@ -18,6 +18,18 @@ const taskSchema = new mongoose.Schema({
 }, {
     timestamps: true
 });
+
+taskSchema.methods.toJSON = function () {
+    const task = this;
+    const taskObject = task.toObject();
+    delete taskObject._id;
+    delete taskObject.owner;
+    delete taskObject.createdAt;
+    delete taskObject.updatedAt;
+    delete taskObject.__v;
+    return taskObject;
+};
+
 const Task = mongoose.model('Task', taskSchema);
 
 module.exports = Task;
